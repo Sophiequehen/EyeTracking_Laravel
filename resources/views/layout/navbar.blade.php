4fc3445b11089e45fe68682e392c3edf872182ad
Sophiequehen <sophie.quehen@gmail.com>
2018-08-23 10:57:13 +0200

    logo's placement modification + home bug set by adding Auth::check(), adding modifier button for superadmins and concerned admins + adding section Vos Bd

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,21 +1,23 @@
-<div id="navbar">
-	<div class='eyes-logo-container'>
-		<div class="left">
-			<div class="lashes"></div>
-			<div class='eye'></div>
-			<div class='eyeclosed'></div>
+		<div id="logo-eyes">
+			<div class='eyes-logo-container'>
+				<div class="left">
+					<div class="lashes"></div>
+					<div class='eye'></div>
+					<div class='eyeclosed'></div>
+				</div>
+				<div class="right">
+					<div class="lashes"></div>
+					<div class='eye'></div>
+					<div class='eyeclosed'></div>
+				</div>
+			</div>
 		</div>
-		<div class="right">
-			<div class="lashes"></div>
-			<div class='eye'></div>
-			<div class='eyeclosed'></div>
+		<div id="navbar">
+			<a href="{{ url('/') }}">Accueil</a>
+			<a href="{{ route('comics_index') }}">Catalogue</a>
+			@if(Auth::user()->fk_role_id === 1 || Auth::user()->fk_role_id === 3) 
+			<a href="{{ route('medias') }}">Médias</a>
+			@endif
+			<a href="{{ url('/legalmentions') }}">À propos</a>
 		</div>
-	</div>
-	<a href="{{ url('/') }}">Accueil</a>
-	<a href="{{ route('comics_index') }}">Catalogue</a>
-	@if(Auth::user()->fk_role_id === 1 || Auth::user()->fk_role_id === 3) 
-	<a href="{{ route('medias') }}">Médias</a>
-	@endif
-	<a href="{{ url('/legalmentions') }}">À propos</a>
-</div>
 
--- resources/views/others/home.blade.php
+++ resources/views/others/home.blade.php
@@ -6,15 +6,15 @@ Accueil
 
 @section('content')
 
-<section class="page-titles">
+<!-- <section class="page-titles">
     <h2>Reprendre la lecture</h2>
     <p>/</p>
 </section>
 <section class="containers_catalog">
-
-    <!-- REQUEST TO DISPLAY THE ALREADY READED COMICS ORGANISED BY ASCENDING ORDER -->
-    {{-- @foreach ($comics as $comic)
-        <article class="comics_catalog">
+-->
+<!-- REQUEST TO DISPLAY THE ALREADY READED COMICS ORGANISED BY ASCENDING ORDER -->
+<!-- @foreach ($comics as $comic) -->
+        <!-- <article class="comics_catalog">
             <a href="{{ route('comics_show', $comic->comic_id) }}">
                 <img class="img_catalog" src="{{ $comic->comic_miniature_url }}" alt="cover">
             </a>
@@ -26,16 +26,43 @@ Accueil
                 </ul>
             </div>
 
-        </article>
-        @endforeach --}}
-    </section>
+        </article> -->
+        <!-- @endforeach -->
+        <!-- </section> -->
 
-    <section class="page-titles">
-        <h2>Dernières BD mises en ligne</h2>
+        <section class="page-titles">
+            <h2>Dernières BD mises en ligne</h2>
+            <p>/</p>
+        </section>
+        <section class="containers_catalog">
+            @foreach ($comics_last as $comic)
+            <article class="comics_catalog">
+                <a href="{{ route('comics_show', $comic->comic_id) }}">
+                    <img class="img_catalog" src="{{ $comic->comic_miniature_url }}" alt="cover">
+                </a>
+                <div class="infos_catalog">
+                    <ul>
+                        <li>{{$comic->comic_title}}</li>
+                        <li>{{$comic->comic_author}}</li>
+                        <li>{{$comic->comic_publisher}}</li>
+                    </ul>
+                </div> 
+                <div class="read_edit_catalog">
+                  @if(Auth::check() && $comic->fk_user_id === Auth::user()->id || Auth::check() && Auth::user()->fk_role_id === 3) 
+                  <a  href="{{ route ('comics_update', $comic->comic_id ) }}" id="button_edit_catalog"><button class="btn-catalogue">Modifier</button></a>
+                  @endif 
+              </div>
+          </article>
+          @endforeach
+      </section>  
+      <!-- TO SEE BD CREATED BY THE CONNECTED ADMIN -->
+      <section class="page-titles">
+        <h2>Vos bandes dessinées</h2>
         <p>/</p>
     </section>
     <section class="containers_catalog">
-        @foreach ($comics_last as $comic)
+        @foreach ($comics_all as $comic)
+        @if(Auth::check() && $comic->fk_user_id === Auth::user()->id) 
         <article class="comics_catalog">
             <a href="{{ route('comics_show', $comic->comic_id) }}">
                 <img class="img_catalog" src="{{ $comic->comic_miniature_url }}" alt="cover">
@@ -46,12 +73,14 @@ Accueil
                     <li>{{$comic->comic_author}}</li>
                     <li>{{$comic->comic_publisher}}</li>
                 </ul>
-            </div>
-        </article>
-        @endforeach
-    </section>  
-
-    <!-- TO SEE BD CREATED BY THE CONNECTED ADMIN -->
-
-
-    @endsection
+            </div> 
+            <div class="read_edit_catalog">
+              @if(Auth::check() && $comic->fk_user_id === Auth::user()->id || Auth::check() && Auth::user()->fk_role_id === 3) 
+              <a  href="{{ route ('comics_update', $comic->comic_id ) }}" id="button_edit_catalog"><button class="btn-catalogue">Modifier</button></a>
+              @endif 
+          </div>
+      </article>
+      @endif
+      @endforeach
+  </section>
+  @endsection
